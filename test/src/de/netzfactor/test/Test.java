b31d4a640cddf98dfacd0f16898827228fb354b9
koelker <koelker@nb-koelker.nf-int.local>
2018-07-20 12:51:33 +0200

    jgf

--- test/src/de/netzfactor/test/Test.java
+++ test/src/de/netzfactor/test/Test.java
@@ -27,7 +27,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at2222222222222: ").append(request.getContextPath());
+		response.getWriter().append("Served 3333: ").append(request.getContextPath());
 	}
 
 	/**
